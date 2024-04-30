139b354295d08037510d56c8894ebe53a34cc94c
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-30 12:05:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//your JS code here. If required.
-//your JS code here. If required.
-const para = document.getElementById('status');
-function buttonClick(){
-	const head = document.createElement("h1");
-	para.innerText="Entered Metaverse";
-	head.innerText = para.innerText;
+function enter(){
+	document.getElementById('status').innerHTML = '<h1>Entered Metaverse</h1>';
 }
