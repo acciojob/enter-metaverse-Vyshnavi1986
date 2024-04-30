5aff201b3f6d7919b60f8010d9e53324352fbeb5
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-04-30 11:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const para = document.getElementById('status');
+function buttonClick(){
+	const head = document.createElement("h1");
+	para.innerText="Entered Metaverse";
+	head.innerText = para.innerText;
+}
